0b5ff3fa379d47d7e671c1a5800bef04d8e20015
Friedrich Schneider <f.j.schneider90@gmail.com>
2025-11-03 11:50:10 +0100

    other examples added

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,12 @@ import pandas as pd
 
 ### Streamlit app
 st.title("Hello world!")
-
+st.subheader("Trying code examples: ")
 st.text("This is text\n[and more text](that's not a Markdown link).")
+
+st.badge("New")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
+)
