3dafb0b293a09bbd13ed9bde017d678d253b1acd
Friedrich Schneider <f.j.schneider90@gmail.com>
2025-11-03 11:38:41 +0100

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+
+### labraries
+import streamlit as st
+import pandas as pd
+
+
+### global variables
+    # prepared data
+    # data = pd.read_csv("data.csv") 
+
+### Functions
+
+
+
+
+### Streamlit app
+st.title("Hello world!")
+
+st.text("This is text\n[and more text](that's not a Markdown link).")
